27cbdb486323c17b075673af8917fbda130010e9
smsanagustin <smsanagustin@up.edu.ph>
2023-06-10 21:18:05 +0800

    copied changes in groups.py from elvin's branch

--- CMSC 127 Project Files/groups.py
+++ CMSC 127 Project Files/groups.py
@@ -1,47 +1,169 @@
-groupInstance = {
-    #TODO: Fill this up w/ group attributes/columns
-    'groupID': '',
-    'name' : ''
-}
+import mysql.connector
 
-def addGroup(populatedGroups):
-    print("Add group still WIP!")
+mariadb_connection = mysql.connector.connect(
+    user="root",
+    password="ilove127",
+    host="localhost",
+    database="cmsc127group3")
 
-    #TODO: Add group inputs here
+cur = mariadb_connection.cursor()
 
-def deleteGroup(populatedGroups):
-    print("Delete group still WIP!")
+def getGroups():
+    groups = {}
+    try: 
+        #TODO: insert new user tuple sql query here
+        query = f"SELECT * FROM grp"
+        cur.execute(query)
+        for group in cur:
+            groups[group[0]] = group[1]
+        return groups
+    except mysql.connector.Error as e: 
+        print(f"Error: {e}")    
 
-    #TODO: Request for the groupID to be deleted
+def viewAllGroups():
+    groups = getGroups()
+    
+    for key, value in groups.items():
+        print(key, "-", value)
 
-def searchGroup(populatedGroups):
-    print("Search group still WIP!")
+def createGroup(userChoice):
+    print("\t-----ADDING-----\n")
+    group_name = input("Enter groupname: ")
+    try: 
+        query = f"INSERT INTO grp (group_name) VALUES ('{group_name}')"
+        cur.execute(query) 
+        query = f"SELECT MAX(group_id) FROM grp"
+        cur.execute(query)
+        idTuple = cur.fetchone()
+        latestGroupId = idTuple[0]
+        query = f"INSERT INTO belongsTo (user_id,group_id) VALUES ({userChoice},{latestGroupId})"
+        cur.execute(query)
+        mariadb_connection.commit()
+        print(group_name,"has been created")
+    except mysql.connector.Error as e: 
+        print(f"Error: {e}")
 
-    #TODO: Request for the groupID to be searched
-    #then display the details of the group
+def deleteGroup():
+    groups = getGroups()
+    for key, value in groups.items():
+        print(key,"-",value)
 
-def updateGroup(populatedGroups):
-    print("Update group still WIP!")
-
-    #TODO: Request for the groupID to be updated
-    #then display input fields for the new values
-
-def addFriendtoGroup(populatedGroups):
-    print("Update group still WIP!")
-
-    #TODO: Request for the groupID to be updated
-    #TODO: Request for the userID to be added
-    #then display prompt if successful
+    print("\t-----DELETING-----\n")
+    groupToDelete = int(input("Enter group id: "))
+    if groupToDelete not in groups.keys():
+        print("The group is not existing!")
+    else:
+        query = f"DELETE FROM grp WHERE group_id = {groupToDelete}"
+        cur.execute(query)
+        mariadb_connection.commit()
+        print("Succecfully deleted group",groups[groupToDelete])
 
 
-def groupsManager(userChoice, populatedGroups):
+def searchGroup():
+    groups = getGroups()
+
+    print("\t-----SEARCHING-----\n")
+    groupToDisplay = int(input("Enter group id: "))
+
+    if groupToDisplay not in groups.keys():
+        print("The group is not existing!")
+    else:
+        print(groupToDisplay,"-",groups[groupToDisplay])
+
+def updateGroup(userChoice):
+    groups = getGroups()
+    print("\t-----UPDATING-----\n")
+    for key, value in groups.items():
+        print(key,"-",value)
+    group_id = int(input("Enter group id: "))
+
+    if group_id not in groups.keys():
+        print("The group is not existing!")
+    else:
+        print(f"\t-----UPDATING {groups[group_id]}-----\n")
+        while True:
+            print(f"[1] Add a friend to a {groups[group_id]}")
+            print(f"[2] Remove a friend from {groups[group_id]}")
+            print(f"[3] Rename {groups[group_id]}")
+            print("[0] Back")
+            action = int(input("Enter the number of the action you wish to perform: "))
+
+            if action == 0:
+                break
+            elif action == 1:
+                addFriendToGroup(userChoice, groups[group_id])
+            elif action == 2:
+                removeFriendFromGroup()
+            elif action == 3:
+                renameGroup(group_id, groups[group_id])
+            else:
+                print("Invalid Choice!")
+
+
+    # Rename the group, add friend to the group, remove friend to a group
+
+def addFriendToGroup(userId, groupId, groupName):
+    import friends
+    friendsOfUser = friends.getFriends(userId)
+    for key,value in friendsOfUser.items():
+        print(key, "-", value)
+
+    friendId = int(input("Enter friend ID: "))
+    if friendId in friendsOfUser.keys():
+        query = f"INSERT INTO belongsTo (user_id,group_id) VALUES ({friendId},{groupId})"
+        cur.execute(query)
+        print(f"{friendsOfUser[friendId]} has been added to {groupName}")
+    else:
+        print("Friend does not exist!")
+
+
+def removeFriendFromGroup(userId, groupId, groupName):
+    import friends
+    friendsOfUser = friends.getFriends(userId)
+    for key,value in friendsOfUser.items():
+        print(key, "-", value)
+    friendId = int(input("Enter friend ID: "))
+
+    if friendId in friendsOfUser.keys():
+        query = f"DELETE FROM belongsTo (user_id,group_id) VALUES ({friendId},{groupId})"
+        cur.execute(query)
+        print(f"{friendsOfUser[friendId]} has been removed from {groupName}")
+        # should catch the case where the friend cannot be removed from the group if the group still
+        #  has an unsettled expense
+    else:
+        print("Friend does not exist!")
+
+def renameGroup(groupId, oldGroupName):
+    newGroupName = input("Enter new group name: ")
+    query = f"UPDATE grp SET group_name = '{newGroupName}' WHERE group_id = {groupId}"
+    cur.execute(query)
+    mariadb_connection.commit()
+    print(f"Group {oldGroupName} has been successfully renamed to {newGroupName}")
+
+def viewAllGroupsWithOB(userId):
+    query = f"""SELECT group_name FROM grp JOIN group_has_expense as `g`
+    ON grp.group_id = g.group_id
+    JOIN expense as `e`
+    ON g.expense_id = e.expense_id
+    JOIN belongsTo as `b`
+    ON g.group_id = b.group_id AND b.user_id = {userId}
+    WHERE e.cash_flow > 0 AND e.isSettled = 0 GROUP BY grp.group_id;"""
+    cur.execute(query)
+
+    print("Groups with outstanding balance")
+    for row in cur.fetchall():
+        print(row[0])
+
+        
+def groupsManager(userChoice, userName):
     while True:
         print("\n What would you like to do?\n"
             "[1] Add Group\n"
             "[2] Delete Group\n"
             "[3] Search Group\n"
-            "[4] Update Group\n"
-            "[5] Add Friend to a Group\n"
+            "[4] View All Groups\n"
+            "[5] Update Group\n"
+            "[6] View All Groups with an Outstanding Balance\n"
             "[0] Back"
             )
         groupManagerOption = input("\nEnter choice: ")
@@ -49,41 +171,19 @@ def groupsManager(userChoice, populatedGroups):
         if groupManagerOption == '0':
 
             import signupLoginMenu
-            signupLoginMenu.mainPage(userChoice)
+            signupLoginMenu.mainPage(userChoice,userName)
             break
         elif groupManagerOption == '1':
-            addGroup(populatedGroups)
-
-            import signupLoginMenu
-            signupLoginMenu.mainPage(userChoice)
-            break
+            createGroup(userChoice)
         elif groupManagerOption == '2':
-            deleteGroup(populatedGroups)
-
-            import signupLoginMenu
-            signupLoginMenu.mainPage(userChoice)
-            break
+            deleteGroup()
         elif groupManagerOption == '3':
-            searchGroup(populatedGroups)
-
-            import signupLoginMenu
-            signupLoginMenu.mainPage(userChoice)
-            break
+            searchGroup()
         elif groupManagerOption == '4':
-            updateGroup(populatedGroups)
-
-            import signupLoginMenu
-            signupLoginMenu.mainPage(userChoice)
-            break
+            viewAllGroups()
         elif groupManagerOption == '5':
-            addFriendtoGroup(populatedGroups)
-
-            import signupLoginMenu
-            signupLoginMenu.mainPage(userChoice)
-            break
+            updateGroup(userChoice)
+        elif groupManagerOption == '6':
+            viewAllGroupsWithOB(userChoice)
         else:
             print("Invalid Input!")
-
-            import signupLoginMenu
-            signupLoginMenu.mainPage(userChoice)
-            break
